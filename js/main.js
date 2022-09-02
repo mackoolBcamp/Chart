9cefc431bcfdbb17818c9bc23721bc5a98257a33
Luis <le.cervantes.96@gmail.com>
2022-09-02 11:24:43 -0500

    Updates Chart

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 const countrySelectElement = document.querySelector(".country_option");
 const citiesSelect = document.getElementById("allCities")
 
-
-
 function getCovidData() { }
-
-
 
 async function apiCall() {
   axios
     .get("https://coronavirus.m.pipedream.net")
     .then((response) => {
+      //Crea Array de los paises
       const citiesArray = []
       for (let index = 0; index < response.data.rawData.length; index++) {
         citiesArray.push(response.data.rawData[index].Country_Region)
@@ -24,8 +21,8 @@ async function apiCall() {
         <option value="${element}">${element}</option>
         </select>`
       });
-      let index = 0
 
+      //Establece la gráfica
       function displayChart() {
         const labels = ["Recovered", "Deaths",];
         const data = {
@@ -33,13 +30,12 @@ async function apiCall() {
           datasets: [
             {
               label: "Casos diarios",
-              backgroundColor: "rgb(10, 70, 126)",
+              backgroundColor: ["#DC002D","#30FF54"],
               borderColor: "rgb(255, 99, 132)",
               data: [10, 20],
             },
           ],
         };
-
         const config = {
           type: "doughnut",
           data: data,
@@ -47,21 +43,22 @@ async function apiCall() {
         };
         const myChart = new Chart(document.getElementById("myChart"), config);
         console.log("se ejecuto funcion");
+        //Actualza la gráfica
         const setData = () => {
           response.data.rawData.map(element => {
             if (element.Country_Region == citiesSelect.value) {
                index = response.data.rawData.indexOf(element)
             }
           })
-          console.log(index)
+          console.log(response.data.rawData[index])
           console.log(citiesSelect.value)
-          myChart.data.datasets[0].data[0]=0
-          console.log(myChart.data.datasets[0].data[0])
+          myChart.data.datasets[0].data[0]=(response.data.rawData[index].Confirmed-response.data.rawData[index].Deaths)
+          myChart.data.datasets[0].data[1]=response.data.rawData[index].Deaths
+          console.log("Casos recuperados: ",myChart.data.datasets[0].data[0])
+          console.log("Muertes: ",myChart.data.datasets[0].data[1])
+          myChart.update()
         }
         document.getElementById("btn").addEventListener("click", setData)
-
-        console.log(myChart.data.datasets[0].data[0])
-
       }
       displayChart();
 
